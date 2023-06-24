662e3d4d28be31406d1f9d1d3205b236add76d71
mashhhhha <110398842+mashhhhha@users.noreply.github.com>
2023-06-24 20:40:39 +0300

    dev_38

--- todolist/asgi.py
+++ todolist/asgi.py
@@ -9,6 +9,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to_do_app.settings')
 
 application = get_asgi_application()
